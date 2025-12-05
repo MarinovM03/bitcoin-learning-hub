f3082defcc3a82ba47fbe344e85e1e340a22763f
Martin Marinov <martin.marinov10@yahoo.com>
2025-12-05 13:36:45 +0000

    Add Header component and styles
    React Router is also installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { Routes } from "react-router";
+import Header from "./components/header/Header";
 
 function App() {
     return (
         <>
+            <Header />
+
             <h1>Bitcoin Learning Hub</h1>
         </>
-    )
+    );
 }
 
 export default App
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                <Link className="home" to="/">Bitcoin Learning Hub</Link>
+            </h1>
+
+            <nav>
+                <Link to="/articles">All Articles</Link>
+
+                <div id="user">
+                    <Link to="/create">Create Article</Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+
+                <div id="guest">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            </nav>
+        </header>
+    );
+}
