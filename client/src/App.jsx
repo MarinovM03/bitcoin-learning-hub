f468b2cf8fbf939c814c54a9c5a653e9e1ad9486
Martin Marinov <martin.marinov10@yahoo.com>
2025-12-06 21:11:45 +0000

    Update App.jsx and Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import * as authService from "./services/authService";
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
@@ -6,18 +9,59 @@ import Catalog from "./components/catalog/Catalog";
 import Details from "./components/details/Details";
 import Create from "./components/create/Create";
 import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
+    const navigate = useNavigate();
+    const [auth, setAuth] = useState({});
+
+    useEffect(() => {
+        const serializedAuth = localStorage.getItem('auth');
+        if (serializedAuth) {
+            setAuth(JSON.parse(serializedAuth));
+        }
+    }, []);
+
+    const loginSubmitHandler = async (values) => {
+        try {
+            const result = await authService.login(values.email, values.password);
+            setAuth(result);
+            localStorage.setItem('auth', JSON.stringify(result));
+            navigate('/');
+        } catch (err) {
+            console.log("Login failed", err.message);
+        }
+    };
+
+    const registerSubmitHandler = async (values) => {
+        try {
+            const result = await authService.register(values.email, values.password);
+            setAuth(result);
+            localStorage.setItem('auth', JSON.stringify(result));
+            navigate('/');
+        } catch (err) {
+            console.log("Register failed", err.message);
+        }
+    };
+
+    const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('auth');
+        navigate('/');
+    };
+
     return (
         <>
-            <Header />
+            <Header auth={auth} onLogout={logoutHandler} />
 
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/articles" element={<Catalog /> } />
                 <Route path="/articles/:articleId/details" element={<Details />} />
                 <Route path="/articles/create" element={<Create />} />
-                <Route path="/login" element={<Login />} />
+
+                <Route path="/login" element={<Login onLoginSubmit={loginSubmitHandler} />} />
+                <Route path="/register" element={<Register onRegisterSubmit={registerSubmitHandler} />} />
             </Routes>
 
             <Footer />
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router";
 
-export default function Header() {
+export default function Header({
+    auth,
+    onLogout,
+}) {
+    const isAuthenticated = !!auth.accessToken;
+
     return (
         <header>
             <h1>
@@ -10,15 +15,17 @@ export default function Header() {
             <nav>
                 <Link to="/articles">All Articles</Link>
 
-                <div id="user">
-                    <Link to="/articles/create">Create Article</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {isAuthenticated ? (
+                    <div id="user">
+                        <Link to="/articles/create">Create Article</Link>
+                        <Link to="/" onClick={onLogout}>Logout</Link>
+                    </div>
+                ) : (
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                )}
             </nav>
         </header>
     );
