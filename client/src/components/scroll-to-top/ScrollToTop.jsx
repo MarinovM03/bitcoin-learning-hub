289ec9244096e771b78f9c6f4175ba78b873a249
Martin Marinov <martin.marinov10@yahoo.com>
2026-02-09 21:38:47 +0000

    Enhance scroll-to-top button styling & icon

--- client/src/components/scroll-to-top/ScrollToTop.jsx
+++ client/src/components/scroll-to-top/ScrollToTop.jsx
@@ -20,7 +20,6 @@ export default function ScrollToTop() {
 
     useEffect(() => {
         window.addEventListener("scroll", toggleVisibility);
-        
         return () => window.removeEventListener("scroll", toggleVisibility);
     }, []);
 
@@ -28,7 +27,9 @@ export default function ScrollToTop() {
         <div className="scroll-to-top">
             {isVisible && (
                 <div onClick={scrollToTop} className="scroll-btn">
-                    ⬆
+                    <svg className="scroll-icon" viewBox="0 0 24 24">
+                        <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/>
+                    </svg>
                 </div>
             )}
         </div>
