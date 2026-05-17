b8e1ae82d1afe72bc514413a32acb4cde4cf5d1a
Martin Marinov <martin.marinov10@yahoo.com>
2026-05-17 18:28:19 +0100

    chore(server): configure vitest with mongo-memory pool

--- server/vitest.config.js
+++ server/vitest.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
         environment: 'node',
         setupFiles: ['./test/setup.js'],
         testTimeout: 30_000,
-        hookTimeout: 60_000,
+        hookTimeout: 600_000,
         pool: 'forks',
         poolOptions: { forks: { singleFork: true } },
+        isolate: false,
     },
 });
