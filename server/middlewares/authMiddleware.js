f8af4aaa7eab43ac2d5ffbb8b7005231139c4a9c
Martin Marinov <martin.marinov10@yahoo.com>
2026-06-27 20:24:41 +0100

    build(server): add eslint with a node flat config
    Adds eslint plus a flat config for the server and fixes the two issues it surfaces: an unused catch binding and a destructure-omit in the search route.

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ export const authMiddleware = (req, res, next) => {
             const decodedToken = jwt.verify(token, SECRET);
             req.user = decodedToken;
             next();
-        } catch (err) {
+        } catch {
             req.user = undefined;
             next();
         }
