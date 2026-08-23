051b982a5e041db369e2e30e9023d790965acf00
Martin Marinov <martin.marinov10@yahoo.com>
2026-08-23 23:49:53 +0300

    perf(auth): cap the stored revoked-session list
    revokedSessions is loaded on every authenticated request and only shrank
    on logout. Pushes now keep the newest 50 entries.

--- server/utils/sessionRevocation.js
+++ server/utils/sessionRevocation.js
@@ -1,6 +1,8 @@
 import User from '../models/User.js';
 import { SESSION_MAX_AGE_MS } from './authCookies.js';
 
+const MAX_REVOKED_SESSIONS = 50;
+
 export const revokeSession = async (tokenPayload) => {
     const { _id, sid, exp } = tokenPayload;
 
@@ -12,7 +14,14 @@ export const revokeSession = async (tokenPayload) => {
     const expiresAt = exp ? new Date(exp * 1000) : new Date(Date.now() + SESSION_MAX_AGE_MS);
 
     await User.updateOne({ _id }, { $pull: { revokedSessions: { expiresAt: { $lte: new Date() } } } });
-    await User.updateOne({ _id }, { $push: { revokedSessions: { sid, expiresAt } } });
+    await User.updateOne({ _id }, {
+        $push: {
+            revokedSessions: {
+                $each: [{ sid, expiresAt }],
+                $slice: -MAX_REVOKED_SESSIONS,
+            },
+        },
+    });
 };
 
 export const isSessionRevoked = (user, sid) =>
